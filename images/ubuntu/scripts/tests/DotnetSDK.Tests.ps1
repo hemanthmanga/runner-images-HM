6d854a41b38fab4506fad83bc78a0562ba1160b3
hemanthmanga <hemanthmanga@github.com>
2024-11-14 15:12:26 +0530

    [Ubuntu] Adding dotnet latest version 9*****

--- images/ubuntu/scripts/tests/DotnetSDK.Tests.ps1
+++ images/ubuntu/scripts/tests/DotnetSDK.Tests.ps1
@@ -20,11 +20,11 @@ Describe "Dotnet and tools" {
             $dotnet = @{ dotnetVersion = $version }
 
             It "SDK <dotnetVersion> is available" -TestCases $dotnet {
-                $dotnetSDKs | Should -Match "$dotnetVersion\.\d+(\.\d+)*"
+                $dotnetSDKs | Should -Match "\b$($dotnetVersion)\b"
             }
 
             It "Runtime <dotnetVersion> is available" -TestCases $dotnet {
-                $dotnetRuntimes | Should -Match "$dotnetVersion\.\d+(\.\d+)*"
+                $dotnetRuntimes | Should -Match "\b$($dotnetVersion)\b"
             }
         }
     }
